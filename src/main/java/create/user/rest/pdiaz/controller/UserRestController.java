3b3580f4c504eb5b594e2fbfaf9dc3751897c679
Paul Diaz <paulgiancarlod@gmail.com>
2020-02-19 23:26:45 -0300

    controller

--- src/main/java/create/user/rest/pdiaz/controller/UserRestController.java
+++ src/main/java/create/user/rest/pdiaz/controller/UserRestController.java
@@ -2,14 +2,26 @@ package create.user.rest.pdiaz.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import create.user.rest.pdiaz.model.User;
+
 
 @RestController("/")
 public class UserRestController {
 
     @GetMapping
-    public String helloGradle() {
+    public String registeredUser() {
         return "Hello Gradle!";
     }
+    @PostMapping(value="/userreg/")
+    public User postMethodName(@Valid @RequestBody User entity) {
+        
+        
+        return entity;
+    }
+    
 
 }
